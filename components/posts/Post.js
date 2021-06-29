deb11a0d8be2e9e3d10fa3a2767d462fc0d3c053
sam <sam.leider@hotmail.com>
2021-06-29 07:11:20 -0400

    change post text to array of text

--- components/posts/Post.js
+++ components/posts/Post.js
@@ -4,9 +4,6 @@ export default function Post({ post }) {
   const date = new Date(post.created_on.full_date)
   console.log(date.getTime())
   const [drawer, setDrawer] = useState(false)
-  let text = `
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus facere et architecto neque magnam quis iusto eligendi, quasi tempore fugit alias inventore quo sit molestias molestiae reprehenderit accusantium suscipit exercitationem.
-  `
   return (
     <article className='flex  p-2 my-2 bg-gray-50 rounded shadow'>
       <figure className="flex flex-col min-w-max mr-2 items-center border">
@@ -15,9 +12,9 @@ export default function Post({ post }) {
       <section className='flex flex-col justify-between w-full h-full p-2'>
         <small className='pl-1 mb-2'>{post.user.name} <time className="text-xs text-gray-500 ml-2" dateTime={date.toISOString()}>{post.created_on.date}</time></small>
         <p className="leading-12 px-1 py-1 border-b">
-          {(post.text.length > 50 ) && !drawer ? (post.text.substr(0,50) + '...') : (post.text)}  
+          {(post.text.length > 1 ) && !drawer ? (post.text.substr(0,50) + '...') : (post.text)}  
         </p>
-          {(post.text.length > 300 ) && (
+          {(post.text.length > 1 ) && (
             <div className='my-2 mx-auto flex justify-center cursor-pointer w-10' onClick={() => setDrawer(old => !old)}>
                 drawer ? (
                   <i className="fas fa-chevron-down"  />
